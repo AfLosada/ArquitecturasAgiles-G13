9d3211a442330ca19091e57c73c80ef0b3bf4bc8
e.chaparroa-uniandes <e.chaparroa@uniandes.edu.co>
2024-02-24 09:38:11 -0500

    registro y consulta - version inicial

---
+++ experimento-1/consulta_usuario/app.py
@@ -0,0 +1,21 @@
+# app_query.py para el servicio de consulta
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Datos de usuarios (simulación de base de datos)
+coreos_registrados = ["correo1@gmail.com", "correo2@gmail.com"]
+
+@app.route('/check_user', methods=['POST'])
+def check_user():
+    data = request.json
+    correo = data.get("correo")
+
+    if correo in coreos_registrados:
+        return jsonify({"can_register": False, "message": "El usuario ya existe"})
+    else:
+        return jsonify({"can_register": True, "message": "El usuario puede ser registrado"})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5001)
