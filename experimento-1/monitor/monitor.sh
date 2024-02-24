db667f3673899cafdee41b91870dbe331ed16e69
jcamilovallejos <j.vallejosg@uniandes.edu.co>
2024-02-24 16:02:01 -0500

    Adicion de fall back service

--- experimento-1/monitor/monitor.sh
+++ experimento-1/monitor/monitor.sh
@@ -10,17 +10,23 @@ while true; do
         if docker inspect server-y > /dev/null 2>&1 && docker ps -q -f name=server-y > /dev/null 2>&1; then
             echo "El servidor Y está respondiendo, pero x ahora está en línea."
             echo "Apagando el servidor Y..."
-            docker restart server-y
-            echo "Servidor Y reiniciado."
+            docker stop server-y
+            docker rm server-y
+            echo "Servidor Y apagado."
             SERVER_Y_CREATED=false
         else
             echo "El servidor Y se encuentra apagado o no existe."
         fi
 
     else
-        docker restart $SERVER_X_NAME
-        echo "Nuevo servidor Y creado y en ejecución porque el servidor X esta apagado."
-        SERVER_Y_CREATED=true
+
+        if [ "$SERVER_Y_CREATED" = false ]; then
+            docker run -d --name "server-y" "$SERVER_X_IMAGE" /bin/sh -c "while :; do sleep 10; done"
+            echo "Nuevo servidor Y creado y en ejecución porque el servidor X esta apagado."
+            SERVER_Y_CREATED=true
+        else
+            echo "El servidor X no responde, pero el servidor Y ya fue creado y está en ejecución."
+        fi  
     fi
 
     # Esperar 10 segundos antes de realizar el próximo ping
