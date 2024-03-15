8d59bbec72e54dd4cd2f3fb73a64fa559473cddb
jcamilovallejos <j.vallejosg@uniandes.edu.co>
2024-03-14 22:00:07 -0500

    Anteriores requirements txt

--- experimento-2/certificador/app.py
+++ experimento-2/certificador/app.py
@@ -1,13 +1,10 @@
 from flask import Flask, jsonify
-from flask_jwt_extended import JWTManager, create_access_token
 
 app = Flask(__name__)
-app.config['JWT_SECRET_KEY'] = 'grupo_17_secret_key'
-jwt = JWTManager(app)
 
 @app.route('/token')
 def generate_token():
-  token = create_access_token(identity="certificator")
+  token = 'token'
   return jsonify(token=token)
 
 if __name__ == '__main__':
