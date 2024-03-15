e6e824e7d10dc702b3eba9f53eeb2f25387ef7db
Cristian Arnulfo Arias Vargas <142316997+CristianAAV@users.noreply.github.com>
2024-03-15 17:19:39 -0500

    Read.me API GETWAY

--- experimento-1/gatling/load-simulator/src/test/scala/usuariosConcurrentes/CargaUsuarios.scala
+++ experimento-1/gatling/load-simulator/src/test/scala/usuariosConcurrentes/CargaUsuarios.scala
@@ -13,7 +13,7 @@ import java.util.concurrent.ThreadLocalRandom
  */
 class UsuariosConcurrentes extends Simulation {
   val httpConf = http
-    .baseUrl("http://192.168.1.11:5000") // Set your target base URL
+    .baseUrl("http://localhost:5000/user-commands/users/register") // Set your target base URL
 
   val feeder = Iterator.continually(Map(
     "value1" -> scala.util.Random.nextInt(1000) // Generate a random number for value2
